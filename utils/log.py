9b36d6308be893bfdbb2022b709a4d0fb321756d
Leeloo <164774099+criticalsool@users.noreply.github.com>
2024-10-11 23:32:23 +0200

    Update log.py
    Add fileHandler to log to file

--- utils/log.py
+++ utils/log.py
@@ -1,18 +1,27 @@
 import logging
 from rich.logging import RichHandler
 
+
 def init_logging(args):
+    level=logging.DEBUG
+    logfileFormat = "%(asctime)s %(levelname)-8s %(name)-8s %(message)s"
+    logfileDateFormat = "[%d/%m/%Y %I:%M:%S]"
+
+    fileHandler = logging.FileHandler('log.log')
+    logfileFormatter = logging.Formatter(logfileFormat, logfileDateFormat)
+    fileHandler.setFormatter(logfileFormatter)
+    
     if args.verbose:
         logging.basicConfig(
-            level=logging.DEBUG,
+            level=level,
             format="%(message)s",
-            handlers=[RichHandler(rich_tracebacks=True)],
-            datefmt="%d/%m/%Y %I:%M:%S",
+            handlers=[RichHandler(rich_tracebacks=True), fileHandler],
+            datefmt=logfileDateFormat
         )
     else:
         logging.basicConfig(
             level=logging.INFO,
             format="%(message)s",
-            handlers=[RichHandler(rich_tracebacks=True)],
-            datefmt="%d/%m/%Y %I:%M:%S",
+            handlers=[RichHandler(rich_tracebacks=True), fileHandler],
+            datefmt=logfileDateFormat
         )
